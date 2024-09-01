fecfbc607b3daa6724ff4e5de67e93aa17ec187e
AbhinayChimmula24 <achimmula@raintechnologiesinc.com>
2024-09-01 12:19:03 -0400

    it updates the q table are those right values ? i am not sure

--- src/self_play_v2.py
+++ src/self_play_v2.py
@@ -6,7 +6,7 @@ import sys
 from tic_tac_toe_grid import play_game
 
 
-def q_table():
+def q_table_initialization():
     q_table = [[0 for i in range(10)] for j in range(10)]
     j = 0
     k = 0
@@ -21,7 +21,8 @@ def q_table():
             q_table[0][column_headers] = "X"
         else:
             q_table[0][column_headers] = "O"
-    print(tabulate(q_table, tablefmt="grid"))
+    # print(tabulate(q_table, tablefmt="grid"))
+    return q_table
 
 
 def epsilon_greedy():
@@ -37,29 +38,47 @@ def epsilon_greedy():
 
 sys.setrecursionlimit(10**5)
 
+
+def retrieve_q_value_and_position(state: Dict[str, int], number_of_played_positions: int):
+    q_table = q_table_initialization()
+    value_row_postion = 0
+
+    for i in range(0, len(q_table)):
+        if q_table[i+1][0] == (state['row'], state['column']):
+            value_row_postion = i + 1
+            break
+
+    return q_table[value_row_postion][number_of_played_positions + 1], {
+        'row': value_row_postion, 'column': number_of_played_positions + 1
+    }
+
+
 total_iterations = 0
 
 
 def q_learning(list_of_available_states: List[Dict[str, int]],
                list_of_played_positions: List[Dict[str, int]],
                played_game_status,
+               q_table_to_update,
                ):
 
     alpha = 0.1
     gamma = 0.9
     reward = 0
+    arg_max_list = []
     global total_iterations
     total_iterations += 1  # Increment the global counter
     print("Total iterations:", total_iterations)
+    print('PLAYED GAME STATUS', played_game_status)
 
     if played_game_status is not None:
         if 'winner' in played_game_status:
             if played_game_status["winner"] == "Draw":
                 print("Draw")
-                return 2
+                return 1
             elif played_game_status["winner"] == "X":
                 print("X wins")
-                return 1
+                return 5
             elif played_game_status["winner"] == "O":
                 print("O wins")
                 return -1
@@ -69,10 +88,12 @@ def q_learning(list_of_available_states: List[Dict[str, int]],
         for j in range(3):
             list_of_available_states.append(
                 {"row": i, "column": j})
+
     for played_position in list_of_played_positions:
         list_of_available_states.remove(played_position)
 
     random.shuffle(list_of_available_states)
+
     for available_state in list_of_available_states:
         new_game = play_game()
         played_game_status = next(new_game)
@@ -92,15 +113,53 @@ def q_learning(list_of_available_states: List[Dict[str, int]],
         list_of_new_unplayed_positions = list_of_available_states.copy()
         list_of_new_unplayed_positions.remove(available_state)
 
-        reward = q_learning(list_of_new_unplayed_positions,
-                            list_of_new_played_positions, played_game_status)
+        retrieved_position_and_q_value = retrieve_q_value_and_position(
+            {"row": available_state["row"],
+                "column": available_state["column"]},
+            number_of_played_positions=len(list_of_played_positions)
+        )
+        current_q_value = retrieved_position_and_q_value[0]
+        position_to_update_on_q_table = retrieved_position_and_q_value[1]
+
+        next_state_q_values = q_learning(list_of_new_unplayed_positions,
+                                         list_of_new_played_positions, played_game_status, q_table_to_update)
+
+        q_table_to_update[position_to_update_on_q_table['row']
+                          ][position_to_update_on_q_table['column']] = next_state_q_values
+
+        print(tabulate(q_table_to_update, tablefmt="grid"))
+
+        if next_state_q_values is None:
+            continue
+        else:
+            arg_max_list.append(next_state_q_values)
+
+        print("Current Q value", current_q_value,
+              'argmax list ', arg_max_list, 'reward', reward)
         print("Reward", reward, )
+        q_value = current_q_value + alpha * \
+            (reward + gamma * max(arg_max_list) - current_q_value)
+
+        print("Q value", q_value)
+
         # print("List of new played positions", list_of_new_played_positions)
         # print("List of new unplayed positions",
         #       list_of_new_unplayed_positions)
 
+    q_value = current_q_value + alpha * \
+        (reward + gamma*max(arg_max_list) - current_q_value)
+    return q_value
+
 
 if __name__ == '__main__':
-    # q_table()
-    q_learning([], [],
-               played_game_status=None)
+    q_table_to_update = q_table_initialization()
+    q_learning(list_of_available_states=[], list_of_played_positions=[
+        # {'row': 1, 'column': 1},
+        # {'row': 0, 'column': 1},
+        # {'row': 2, 'column': 0},
+        # {'row': 0, 'column': 2},
+        # {'row': 0, 'column': 0},
+        # {'row': 2, 'column': 2},
+    ],
+        played_game_status=None,
+        q_table_to_update=q_table_to_update)
